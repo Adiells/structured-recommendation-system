f7ed2f407a2b1d3326abf9729344d23b33317af2
Carlos Adauto <carlosadauto7oficial@gmail.com>
2026-03-13 15:34:05 -0300

    file[Adição da matriz produtos]: Adicionei a matriz produtos e refatorei a função geraListaCompras

--- listaCompras.cpp
+++ listaCompras.cpp
@@ -1,11 +1,12 @@
 #include "listaCompras.h"
 
-void gerarListaCompras(char caminho[], vector<string>& clientes, map<string, int>& mapaClientes){
+void gerarListaCompras(char caminho[], vector<string>& clientes, map<string, int>& mapaClientes, vector<string>& produtos, map<int, int>& mapaProdutos, vector<vector<int>>& listaDeCompras){
     FILE *arquivo;
     arquivo = fopen(caminho, "r");
 
     Produto produto;
     char ultimoCliente[9];
+    int ultimoProduto = 0;
 
     fscanf(arquivo, "%*[^\n]\n");
     while(fscanf(arquivo, "%d, %8[^,], %d, %49[^\n]\n", &produto.dataCompra, produto.codeCliente, &produto.codeProduto, produto.nomeProduto) == 4){
@@ -16,10 +17,30 @@ void gerarListaCompras(char caminho[], vector<string>& clientes, map<string, int
             }else{
                 mapaClientes[produto.codeCliente] = mapaClientes[ultimoCliente] + 1;
             }
+
+            listaDeCompras.push_back(vector<int>()); 
         }
         strcpy(ultimoCliente, produto.codeCliente);
+
+        produtos.push_back(produto.nomeProduto);
+        if(mapaProdutos.find(produto.codeProduto) == mapaProdutos.end()){
+            if(mapaProdutos.empty()){
+                mapaProdutos[produto.codeProduto] = 0;
+            }else{
+                mapaProdutos[produto.codeProduto] = mapaProdutos[ultimoProduto] + 1;
+            }
+        }
+        ultimoProduto = produto.codeProduto;
+
+        int indiceCliente = mapaClientes[produto.codeCliente];
+        int indiceProduto = mapaProdutos[produto.codeProduto];
+        
+        listaDeCompras[indiceCliente].push_back(indiceProduto);
+    
     }
+    fclose(arquivo);
 }
+
 
 //Funções de auxílio para acompanhar o desenvolvimento do nosso código
 void imprimirVetor(vector<string>& vetor){
