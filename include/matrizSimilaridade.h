b292e53510a473b5441d57a3de6313c0dbf3b641
Adiells <adielenilson@gmail.com>
2026-03-16 22:10:42 -0300

    fix: corrige cálculo da interseção e loop da matriz de compras

--- include/matrizSimilaridade.h
+++ include/matrizSimilaridade.h
@@ -5,10 +5,4 @@ vector<vector<int>> gerarMatrizComprasGrande (vector<vector<int>> &ListaDeCompra
 
 vector<vector<int>> gerarMatrizIntersecao (vector<vector<int>> &MatrizDeCompras1, int numeroDeClientes, int numDeProdutos);
 
-vector<vector<float>> gerarMatrizSimilaridade(vector<vector<int>> &MatrizIntersecao, vector<vector<int>> &ListaDeCompras);
-
-void imprimirMatrizComprasGrande ( vector<vector<int>>& MatrizComprasGrande);
-
-void imprimirMatrizIntersecao(vector<vector<int>>& MatrizIntersecao);
-
-void imprimirMatrizSimilaridade(vector<vector<float>>& matrizSimilaridade);
+vector<vector<float>> gerarMatrizSimilaridade(vector<vector<int>> &MatrizIntersecao);
--- src/matrizSimilaridade.cpp
+++ src/matrizSimilaridade.cpp
@@ -1,11 +1,12 @@
 #include "listaCompras.h"
+#include "matrizSimilaridade.h"
 
 vector<vector<int>> gerarMatrizComprasGrande (vector<vector<int>> &ListaDeCompras, int numeroDeClientes, int numDeProdutos){
 
     vector<vector<int>> MatrizCompras (numeroDeClientes, vector<int>(numDeProdutos,0));
 
     for (int i = 0; i < numeroDeClientes; i++){
-        for(int j = 0; i < ListaDeCompras[i].size(); j++){
+        for(int j = 0; j < ListaDeCompras[i].size(); j++){
             int produtoComprado = ListaDeCompras[i][j];
             MatrizCompras[i][produtoComprado] = 1;
         }
@@ -13,68 +14,38 @@ vector<vector<int>> gerarMatrizComprasGrande (vector<vector<int>> &ListaDeCompra
     return MatrizCompras;
 }
 
-vector<vector<int>> GerarMatrizIntersecao (vector<vector<int>> &MatrizDeCompras1, int numeroDeClientes, int numDeProdutos) {
+vector<vector<int>> gerarMatrizIntersecao (vector<vector<int>> &MatrizDeCompras1, int numeroDeClientes, int numDeProdutos) {
+    cout << numeroDeClientes << endl;
 
-    vector<vector<int>> MatrizIntersecao (numeroDeClientes, vector<int>(numDeProdutos,0));
+    vector<vector<int>> MatrizIntersecao (numeroDeClientes, vector<int>(numeroDeClientes,0));
 
     for(int i =0; i < numeroDeClientes; i++){
         for(int j = 0; j < numeroDeClientes; j++){
             for(int k = 0 ; k < numDeProdutos; k++){
-                MatrizIntersecao[i][j] += MatrizIntersecao[i][k] * MatrizIntersecao[j][k];
+                // cout << "teste" <<  " " << i << " " << j << " " << k << endl;
+                MatrizIntersecao[i][j] += MatrizDeCompras1[i][k] * MatrizDeCompras1[j][k];
             }
         }
     }
     return MatrizIntersecao;   
 }
 
-vector<vector<float>> GerarMatrizSimilaridade(vector<vector<int>> &MatrizIntersecao, vector<vector<int>> &ListaDeCompras){
+vector<vector<float>> gerarMatrizSimilaridade(vector<vector<int>> &MatrizIntersecao){
     
     int numeroClientes = MatrizIntersecao.size();
 
-    vector<vector<float>> MatrizSimilaridade(numeroClientes, vector<float>(numeroClientes,0.0 ));
+    vector<vector<float>> MatrizSimilaridade(numeroClientes, vector<float>(numeroClientes, 0.0));
 
-    for(int i =0; i < numeroClientes; i++){
-        for(int j=0; j < numeroClientes; j++){
+    for(int i = 0; i < numeroClientes; i++){
+        for(int j = 0; j < numeroClientes; j++){
             int valorIntersecao = MatrizIntersecao[i][j];
-            int totalCompradoCliente = ListaDeCompras[i].size();
+            int totalCompradoCliente = MatrizIntersecao[i][i];
 
-            MatrizSimilaridade[i][j] = 1.0 - ((float)valorIntersecao / (float)totalCompradoCliente);
+            if(totalCompradoCliente > 0)
+                MatrizSimilaridade[i][j] = 1.0 - ((float)valorIntersecao / (float)totalCompradoCliente);
 
 
         }
     }
     return MatrizSimilaridade;
-}
-// Funções auxiliares para acompanhar geração das matrizes
-
-void imprimirMatrizComprasGrande ( vector<vector<int>>& MatrizComprasGrande){
-    int i, j;
-    for(i =0; i < MatrizComprasGrande.size(); i++){
-        cout << "Cliente ["<< i <<"]: ";
-        for(j = 0; j < MatrizComprasGrande[i].size(); j++){
-            cout << MatrizComprasGrande[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void ImprimirMatrizIntersecao(vector<vector<int>>& MatrizIntersecao){
-    int i,j;
-    for(i=0; i < MatrizIntersecao.size(); i++){
-        cout << "Cliente ["<< i <<"] ";
-        for(j=0; j < MatrizIntersecao[i].size(); j++){
-            cout << MatrizIntersecao[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void imprimirMatrizSimilaridade(vector<vector<float>>& matrizSimilaridade){
-    for(int i = 0; i < matrizSimilaridade.size(); i++){
-        cout << "Cliente [" << i << "]: ";
-        for(int j = 0; j < matrizSimilaridade[i].size(); j++){
-            cout << matrizSimilaridade[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
