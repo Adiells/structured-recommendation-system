b094fbc732b1683459cba6f494abda196eb6fc5f
Adiells <adielenilson@gmail.com>
2026-03-21 17:42:53 -0300

    feat: conclusão do módulo de recomendação

--- include/recomendacao.h
+++ include/recomendacao.h
@@ -1,9 +1,22 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+typedef struct{
+    int indice;
+    float score;
+}ItemRanking;
 
 vector<string> listaVizinhos(vector<vector<float>> &similaridade, vector<string> &clientesVector, int indiceCliente);
 
-vector<float> listaRankeamento(vector<vector<float>> &similaridade, vector<string> &vizinhos, vector<vector<int>> &compras, vector<int> &produtos, int indiceCliente);
+vector<float> listaRankeamento(vector<vector<float>> &similaridade, vector<string> &vizinhos, vector<vector<int>> &compras, map<string, int> &mapaClientes, int indiceCliente, int qtdProdutos);
+
+bool ordenarPorScore(const ItemRanking &itemA, const ItemRanking &itemB);
+
+vector<ItemRanking> ordenarVetorRankeamento(vector<float> &rankeamento);
+
+void imprimirNomeProduto(vector<ItemRanking> &produtosOrdenados, int qtdProdutos, vector<string> &nomeProdutos);
+
+void rankeamento(vector<vector<float>> &similaridade, vector<string> &clientesVector, vector<string> &produtosVector, map<string, int> &mapaClientes, vector<vector<int>> &matrizCompras, string codigoCliente, int kProdutos);
--- src/recomendacao.cpp
+++ src/recomendacao.cpp
@@ -10,17 +10,59 @@ vector<string> listaVizinhos(vector<vector<float>> &similaridade, vector<string>
     return vizinhos;
 }
 
-vector<float> listaRankeamento(vector<vector<float>> &similaridade, vector<string> &vizinhos, vector<vector<int>> &compras, vector<int> &produtos, int indiceCliente){
-    vector<float> ranking(produtos.size(), 1.0);
+vector<float> listaRankeamento(vector<vector<float>> &similaridade, vector<string> &vizinhos, vector<vector<int>> &compras, map<string, int> &mapaClientes, int indiceCliente, int qtdProdutos){
+    vector<float> ranking(qtdProdutos, 1.0);
 
-    vector<int> comprou(produtos.size(), 0);
+    vector<int> comprou(qtdProdutos, 0);
+
     for(int i = 0; i < compras[indiceCliente].size(); i++){
         comprou[compras[indiceCliente][i]] = 1;
     }
 
     for(int i = 0; i < vizinhos.size(); i++){
-        for(int j = 0; j < compras[i].size(); j++){
-            //amanha continuo
+        string codigoVizinho = vizinhos[i];
+        int indiceVizinho = mapaClientes[codigoVizinho];
+        for(int j = 0; j < compras[indiceVizinho].size(); j++){
+            int produto = compras[indiceVizinho][j];
+            if(!comprou[produto]){
+                ranking[produto] = ranking[produto]*similaridade[indiceCliente][indiceVizinho];
+            }
         }
     }
+    return ranking;
+}
+
+bool ordenarPorScore(const ItemRanking &itemA, const ItemRanking &itemB){
+    return itemA.score < itemB.score;
+}
+
+vector<ItemRanking> ordenarVetorRankeamento(vector<float> &rankeamento){
+    vector<ItemRanking> vetorOrdenado;
+    
+    for(int i = 0; i < rankeamento.size(); i++){
+        ItemRanking item;
+        item.indice = i;
+        item.score = rankeamento[i];
+        vetorOrdenado.push_back(item);
+    }
+
+    sort(vetorOrdenado.begin(), vetorOrdenado.end(), ordenarPorScore);
+    return vetorOrdenado;
+}
+
+
+void imprimirNomeProduto(vector<ItemRanking> &produtosOrdenados, int qtdProdutos, vector<string> &nomeProdutos){
+    for(int i = 0; i < qtdProdutos; i++){
+        string nomeProduto = nomeProdutos[produtosOrdenados[i].indice];
+        cout << "Produto: " << i+1 << " | " << nomeProduto << endl;
+    }
+}
+
+void rankeamento(vector<vector<float>> &similaridade, vector<string> &clientesVector, vector<string> &produtosVector, map<string, int> &mapaClientes, vector<vector<int>> &matrizCompras, string codigoCliente, int kProdutos){
+    int indiceInternoCliente = mapaClientes[codigoCliente];
+
+    vector<string> vizinhos = listaVizinhos(similaridade, clientesVector, indiceInternoCliente);
+    vector<float> ranking = listaRankeamento(similaridade, vizinhos, matrizCompras, mapaClientes, indiceInternoCliente, produtosVector.size());
+    vector<ItemRanking> rankingOrdenado = ordenarVetorRankeamento(ranking);
+    imprimirNomeProduto(rankingOrdenado, kProdutos, produtosVector);
 }
